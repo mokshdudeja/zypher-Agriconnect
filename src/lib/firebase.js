bc1423949195e35d82935e1becf7167365f04d74
unknown <mokshdudeja6@gmail.com>
2026-04-19 08:47:48 +0530

    security: migrate to environment variables and ignore risky files

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "REMOVED_API_KEY",
-  authDomain: "REMOVED authDomain",
-  projectId: "REMOVED projectId",
-  storageBucket: "REMOVED storageBucket",
-  messagingSenderId: "REMOVED senderId",
-  appId: "REMOVED appId"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
